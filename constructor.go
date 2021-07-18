1e87c2644b8503b2e727b92d0aa6319997fc333e
song.lil <soniced@sina.com>
2021-07-18 17:48:01 +0800

    init

---
+++ constructor.go
@@ -0,0 +1,410 @@
+//
+// Copyright (C) 2020-2021 crazybie@git.com.
+//
+
+/*
+# Constructor
+
+tiny tool to make data-parsing and construction deadly simple.
+please check the unit tests for usage.
+
+## All supported converters:
+
+- from(field)
+- split(sep) / split(sep, converter)
+- map(converter)
+- dict(field)
+- obj(type)
+- group(field) / group(field, reduce)
+- sort(field) / sort(field, desc)
+
+## Performance tips
+
+due to the heavy usage of reflection, it performs much bad than a hand-written loader,
+so it's not a good idea to use it to handle super large tables.
+
+*/
+
+package construtor
+
+import (
+	"errors"
+	"fmt"
+	"github.com/gocarina/gocsv"
+	"reflect"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type context struct {
+	obj       reflect.Value
+	fieldType reflect.Type
+}
+
+var buildInTypes = map[string]reflect.Type{
+	"int":     reflect.TypeOf(0),
+	"int32":   reflect.TypeOf(int32(0)),
+	"int64":   reflect.TypeOf(int64(0)),
+	"float32": reflect.TypeOf(float32(0)),
+	"float64": reflect.TypeOf(float64(0)),
+}
+
+func findType(t string, tp reflect.Type) reflect.Type {
+	for tp.Kind() == reflect.Ptr {
+		tp = tp.Elem()
+	}
+	switch tp.Kind() {
+	case reflect.Map, reflect.Slice:
+		return findType(t, tp.Elem())
+	default:
+		if tp.Name() == t {
+			return tp
+		}
+	}
+	panic(fmt.Sprintf("type not found: %v", t))
+}
+
+type converter func(data reflect.Value, ctx *context) reflect.Value
+
+func panicIf(failed bool, msg string, a ...interface{}) {
+	if failed {
+		panic(fmt.Sprintf(msg, a...))
+	}
+}
+
+func tokenize(input string) []string {
+	tokens := []string{}
+	s := 0
+	for i := 0; i < len(input); i++ {
+		r := input[i]
+		if r == '(' || r == ')' || r == '|' || r == ',' || r == ' ' {
+			if s != i {
+				tokens = append(tokens, input[s:i])
+			}
+			if r != ' ' {
+				tokens = append(tokens, string(r))
+			}
+			s = i + 1
+		}
+	}
+	tokens = append(tokens, "<EOF>")
+	return tokens
+}
+
+// expr: call *{ '|' call }
+// arg: ident | expr
+// call: ident '(' arg *{',' arg} ')'
+func parseConverter(input string) converter {
+	tokens := tokenize(input)
+	cur := 0
+	var call func() converter
+
+	expr := func() converter {
+		r := []interface{}{}
+		if c := call(); c != nil {
+			r = append(r, c)
+		}
+		for tokens[cur] == "|" {
+			cur++
+			r = append(r, call())
+		}
+		if len(r) == 0 {
+			return nil
+		}
+		return newConverter("sequence", r)
+	}
+	arg := func() interface{} {
+		if c := expr(); c != nil {
+			return c
+		}
+		r := tokens[cur]
+		cur++
+		return r
+	}
+	call = func() converter {
+		fnName := tokens[cur]
+		if tokens[cur+1] == "(" {
+			cur += 2
+			args := []interface{}{}
+			for tokens[cur] != ")" {
+				args = append(args, arg())
+				if tokens[cur] == "," {
+					cur++
+				}
+			}
+			cur++
+			return newConverter(fnName, args)
+		}
+		if fn := newConverter(fnName, nil); fn != nil {
+			cur++
+			return fn
+		}
+		return nil
+	}
+	return expr()
+}
+
+func newConverter(funName string, args []interface{}) converter {
+	switch funName {
+	case "sequence":
+		return func(rows reflect.Value, ctx *context) reflect.Value {
+			v := rows
+			for _, op := range args {
+				if v = op.(converter)(v, ctx); !v.IsValid() {
+					break
+				}
+			}
+			return v
+		}
+	case "from":
+		return func(_ reflect.Value, ctx *context) (ret reflect.Value) {
+			ret = ctx.obj.FieldByName(args[0].(string))
+			panicIf(!ret.IsValid(), "invalid filed name: %v", args[0])
+			return
+		}
+	case "split":
+		switch len(args) {
+		case 1:
+			return func(s reflect.Value, ctx *context) (ret reflect.Value) {
+				if s.String() != "" {
+					strs := strings.Split(s.String(), args[0].(string))
+					ret = reflect.ValueOf(strs)
+				}
+				return
+			}
+		case 2:
+			return func(s reflect.Value, ctx *context) (ret reflect.Value) {
+				op := args[1].(converter)
+				if s.String() != "" {
+					strs := strings.Split(s.String(), args[0].(string))
+					for idx, s := range strs {
+						v := op(reflect.ValueOf(s), ctx)
+						if !ret.IsValid() {
+							ret = reflect.MakeSlice(reflect.SliceOf(v.Type()), len(strs), len(strs))
+						}
+						ret.Index(idx).Set(v)
+					}
+				}
+				return
+			}
+		}
+
+	case "int", "int32", "int64", "float32", "float64":
+		return func(valueStr reflect.Value, ctx *context) (ret reflect.Value) {
+			v, err := strconv.ParseFloat(valueStr.String(), 64)
+			panicIf(err != nil, "failed to convert %v to %v", valueStr.String(), funName)
+			return reflect.ValueOf(v).Convert(buildInTypes[funName])
+		}
+	case "map":
+		return func(rows reflect.Value, ctx *context) (ret reflect.Value) {
+			op := args[0].(converter)
+			for i := 0; i < rows.Len(); i++ {
+				v := op(rows.Index(i), ctx)
+				if !ret.IsValid() {
+					ret = reflect.MakeSlice(reflect.SliceOf(v.Type()), rows.Len(), rows.Len())
+				}
+				ret.Index(i).Set(v)
+			}
+			return
+		}
+	case "dict":
+		return func(rows reflect.Value, ctx *context) (ret reflect.Value) {
+			field, ok := rows.Type().Elem().Elem().FieldByName(args[0].(string))
+			panicIf(!ok, "invalid field: %v", args[0])
+			for i := 0; i < rows.Len(); i++ {
+				src := rows.Index(i)
+				key := src.Elem().Field(field.Index[0])
+				if !ret.IsValid() {
+					ret = reflect.MakeMapWithSize(reflect.MapOf(key.Type(), src.Type()), rows.Len())
+				}
+				ret.SetMapIndex(key, src)
+			}
+			return
+		}
+	case "obj":
+		return func(rows reflect.Value, ctx *context) (ret reflect.Value) {
+			ret = reflect.New(findType(args[0].(string), ctx.fieldType))
+			for i := 0; i < rows.Len(); i++ {
+				src := rows.Index(i)
+				dst := ret.Elem().Field(i)
+				dst.Set(src.Convert(dst.Type()))
+			}
+			return
+		}
+	case "group":
+		switch len(args) {
+		case 1:
+			return func(rows reflect.Value, ctx *context) (ret reflect.Value) {
+				field, ok := rows.Type().Elem().Elem().FieldByName(args[0].(string))
+				panicIf(!ok, "invalid field %v", args[0])
+
+				for i := 0; i < rows.Len(); i++ {
+					src := rows.Index(i)
+					key := src.Elem().Field(field.Index[0])
+					if !ret.IsValid() {
+						ret = reflect.MakeMap(reflect.MapOf(key.Type(), reflect.SliceOf(src.Type())))
+					}
+					v := ret.MapIndex(key)
+					if !v.IsValid() {
+						v = reflect.MakeSlice(reflect.SliceOf(src.Type()), 0, 0)
+					}
+					ret.SetMapIndex(key, reflect.Append(v, src))
+				}
+				return
+			}
+		case 2:
+			return func(rows reflect.Value, ctx *context) (ret reflect.Value) {
+				tmp := newConverter(funName, args[:1])(rows, ctx)
+				op := args[1].(converter)
+				for _, k := range tmp.MapKeys() {
+					src := tmp.MapIndex(k)
+					val := op(src, ctx)
+					if !ret.IsValid() {
+						ret = reflect.MakeMap(reflect.MapOf(k.Type(), val.Type()))
+					}
+					ret.SetMapIndex(k, val)
+				}
+				return ret
+			}
+		}
+	case "sort":
+		return func(rows reflect.Value, ctx *context) (ret reflect.Value) {
+			field, ok := rows.Type().Elem().Elem().FieldByName(args[0].(string))
+			fIdx := field.Index[0]
+			panicIf(!ok, "field not found: %v", args[0])
+
+			f64Type := buildInTypes["float64"]
+			compareValues := make([]float64, 0, rows.Len())
+			for i := 0; i < rows.Len(); i++ {
+				compareValues = append(compareValues, rows.Index(i).Elem().Field(fIdx).Convert(f64Type).Float())
+			}
+
+			asc := true
+			if len(args) > 1 && args[1].(string) == "desc" {
+				asc = false
+			}
+			sort.Slice(rows.Interface(), func(i, j int) bool {
+				if asc {
+					return compareValues[i] < compareValues[j]
+				} else {
+					return compareValues[i] > compareValues[j]
+				}
+			})
+			return rows
+		}
+	}
+	return nil
+}
+
+func parseFieldConverters(objType reflect.Type) (ret map[string]converter) {
+	ret = map[string]converter{}
+	for i := 0; i < objType.NumField(); i++ {
+		fieldName := objType.Field(i).Name
+		f := objType.Field(i).Tag.Get("cvt")
+		if f != "" {
+			ret[fieldName] = parseConverter(f)
+		}
+	}
+	return ret
+}
+
+func constrcutObj(objValue reflect.Value, cache *map[string]converter) {
+	panicIf(objValue.Kind() != reflect.Struct, "expect struct")
+
+	if len(*cache) == 0 {
+		*cache = parseFieldConverters(objValue.Type())
+	}
+
+	objType := objValue.Type()
+	ctx := &context{obj: objValue}
+	for i := 0; i < objType.NumField(); i++ {
+		fieldInfo := objType.Field(i)
+		f := objValue.Field(i)
+		if c, ok := (*cache)[fieldInfo.Name]; ok {
+			ctx.fieldType = fieldInfo.Type
+			v := c(f, ctx)
+			if v.IsValid() {
+				f.Set(v)
+			}
+		}
+	}
+}
+
+func constructSlice(sliceValue reflect.Value) {
+	panicIf(sliceValue.Kind() != reflect.Slice, "expect slice")
+	var cache map[string]converter
+	for i := 0; i < sliceValue.Len(); i++ {
+		objValue := sliceValue.Index(i).Elem()
+		constrcutObj(objValue, &cache)
+	}
+}
+
+func Construct(ptr interface{}) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = errors.New(fmt.Sprintf("construct object failed: %v", r))
+		}
+	}()
+
+	v := reflect.ValueOf(ptr)
+	panicIf(v.Kind() != reflect.Ptr, "expect pointer")
+
+	switch v.Elem().Kind() {
+	case reflect.Slice:
+		constructSlice(v.Elem())
+	case reflect.Struct:
+		var cache map[string]converter
+		constrcutObj(v.Elem(), &cache)
+	}
+	return nil
+}
+
+func UnmarshalStringToSlice(slicePtr interface{}, csv string) error {
+	sliceValue := reflect.ValueOf(slicePtr)
+	panicIf(sliceValue.Kind() != reflect.Ptr, "expect pointer to slice")
+	rows := sliceValue.Elem()
+	panicIf(rows.Kind() != reflect.Slice, "expect pointer to slice")
+
+	errChan := make(chan error)
+	c := reflect.MakeChan(reflect.ChanOf(reflect.BothDir, rows.Type().Elem().Elem()), 0)
+	go func() {
+		errChan <- gocsv.UnmarshalToChan(strings.NewReader(csv), c.Interface())
+	}()
+	data := rows
+	for {
+		v, notClosed := c.Recv()
+		if !notClosed || v.Interface() == nil {
+			break
+		}
+		vp := reflect.New(reflect.TypeOf(v.Interface()))
+		vp.Elem().Set(v)
+		data = reflect.Append(data, vp)
+	}
+	select {
+	case err := <-errChan:
+		if err != nil {
+			return err
+		}
+	default:
+	}
+
+	rows.Set(data)
+	return nil
+}
+
+func LoadAndConstruct(objPtr interface{}, slicePtr interface{}, csv string) (retObjPtr interface{}, err error) {
+	err = UnmarshalStringToSlice(slicePtr, csv)
+	if err != nil {
+		return nil, err
+	}
+	err = Construct(slicePtr)
+	if err != nil {
+		return nil, err
+	}
+	err = Construct(objPtr)
+	if err != nil {
+		return nil, err
+	}
+	return objPtr, nil
+}
---
+++ constructor_test.go
@@ -0,0 +1,119 @@
+package construtor
+
+import (
+	"github.com/gocarina/gocsv"
+	"github.com/stretchr/testify/assert"
+	"sort"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+type LevelReward struct {
+	MinLevel int32
+	RewardId int64
+}
+
+type RewardCfg struct {
+	ID      int64
+	Mode    int32
+	Value   int32
+	Reward  int32
+	Reward2 string         // 1:50001,11:50001,16:50001,21:50001,23:50001
+	Rewards []*LevelReward `cvt:"from(Reward2)|split(,)|map(split(:,int32)|obj(LevelReward))|sort(MinLevel)"`
+}
+
+type RewardCfgs struct {
+	Data []*RewardCfg
+	Dict map[int64]*RewardCfg           `cvt:"from(Data)|dict(ID)"`
+	Mode map[int32]map[int64]*RewardCfg `cvt:"from(Data)|group(Mode, dict(ID))"`
+}
+
+var tableCsv = `
+ID,Mode,Value,Reward,Reward2,DESC
+1,1,1,50001,"11:50002,1:50001,16:50003,21:50004,23:50005","第一档奖励"
+2,1,3,50002,"1:50002,11:50002,16:50002,21:50002,23:50002","第二档奖励"
+3,1,6,50003,"1:50003,11:50003,16:50003,21:50003,23:50003","第三档奖励"`
+
+func loadTestCsv(r *RewardCfgs) error {
+	_, err := LoadAndConstruct(r, &r.Data, tableCsv)
+	return err
+}
+
+func Test_basic(t *testing.T) {
+
+	r := &RewardCfgs{}
+	err := loadTestCsv(r)
+	assert.Zero(t, err)
+
+	assert.Equal(t, len(r.Data), 3)
+
+	assert.Equal(t, len(r.Data[0].Rewards), 5)
+	rewards := r.Data[0].Rewards
+	assert.Equal(t, rewards[0].MinLevel, int32(1))
+	assert.Equal(t, rewards[0].RewardId, int64(50001))
+	assert.Equal(t, rewards[1].MinLevel, int32(11))
+	assert.Equal(t, rewards[1].RewardId, int64(50002))
+	assert.Equal(t, rewards[2].MinLevel, int32(16))
+	assert.Equal(t, rewards[2].RewardId, int64(50003))
+	assert.Equal(t, rewards[3].MinLevel, int32(21))
+	assert.Equal(t, rewards[3].RewardId, int64(50004))
+	assert.Equal(t, rewards[4].MinLevel, int32(23))
+	assert.Equal(t, rewards[4].RewardId, int64(50005))
+
+	assert.Equal(t, r.Dict[1].ID, int64(1))
+	assert.Equal(t, r.Dict[1].Value, int32(1))
+
+	assert.Equal(t, r.Dict[2].ID, int64(2))
+	assert.Equal(t, r.Dict[2].Value, int32(3))
+
+	assert.Equal(t, r.Dict[3].ID, int64(3))
+	assert.Equal(t, r.Dict[3].Value, int32(6))
+
+	assert.Equal(t, len(r.Mode), 1)
+	assert.Equal(t, len(r.Mode[1]), 3)
+	assert.Equal(t, r.Mode[1][1], r.Dict[1])
+	assert.Equal(t, r.Mode[1][2], r.Dict[2])
+	assert.Equal(t, r.Mode[1][3], r.Dict[3])
+}
+
+func BenchmarkLoadAndConstruct(b *testing.B) {
+	r := &RewardCfgs{}
+	for i := 0; i < b.N; i++ {
+		loadTestCsv(r)
+	}
+}
+
+func BenchmarkLoadManually(b *testing.B) {
+	ret := &RewardCfgs{
+		Dict: make(map[int64]*RewardCfg),
+		Mode: make(map[int32]map[int64]*RewardCfg),
+	}
+	for i := 0; i < b.N; i++ {
+		gocsv.UnmarshalStringToCallback(tableCsv, func(e RewardCfg) {
+			rs := ret.Mode[e.Mode]
+			if rs == nil {
+				ret.Mode[e.Mode] = make(map[int64]*RewardCfg)
+			}
+
+			rewards := []*LevelReward{}
+			ss := strings.Split(e.Reward2, ",")
+			for _, s := range ss {
+				r := strings.Split(s, ":")
+				level, _ := strconv.Atoi(r[0])
+				rewardId, _ := strconv.Atoi(r[1])
+				rewards = append(rewards, &LevelReward{
+					MinLevel: int32(level),
+					RewardId: int64(rewardId),
+				})
+			}
+			sort.Slice(rewards, func(i, j int) bool {
+				return rewards[i].MinLevel < rewards[j].MinLevel
+			})
+			e.Rewards = rewards
+			v := &e
+			ret.Mode[e.Mode][e.ID] = v
+			ret.Dict[e.ID] = v
+		})
+	}
+}
